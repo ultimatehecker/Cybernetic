dad9108f370980a2a16906720bad45c0193cf914
ultimate_hecker <rohan@halarnkar.com>
2022-01-27 22:24:32 -0500

    getting ready for new release

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const Tracing = require("@sentry/tracing");
 
 Sentry.init({
     dsn: "https://d5ca07bf8f4845adb0bbd96aec2a7d96@o922460.ingest.sentry.io/6123054",
-    release: "0.0.1",
+    release: "0.1.0",
     tracesSampleRate: 1.0,
     integrations: [new Sentry.Integrations.Http({ tracing: true })],
     environment: "development",
