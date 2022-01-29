14fa27eb94d238825817cb7f7810a5a7c5eca94a
ultimate_hecker <rohan@halarnkar.com>
2022-01-29 11:24:22 -0500

    added a footer

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -15,6 +15,7 @@ module.exports = {
 			.setAuthor("Cybernetic's Invite Link", "https://cdn.discordapp.com/avatars/879180094650863727/3040c2fb097ef6a9fb59005cab44626c.webp")
 			.setColor(colors["MainColor"])
             .setDescription("This Cybernetic Invite Link does include the slash commands which will be added soon and includes the regular message based commands.")
+            .setFooter(`Cybernetic Invite Link requested by ${message.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`,message.author.displayAvatarURL())
 
 		message.reply({ embeds: [invite], components: [{ type: "ACTION_ROW", components: [{ type: "BUTTON", label: "Bot Invite", url: "https://discord.com/api/oauth2/authorize?client_id=834177766869565491&permissions=8&scope=applications.commands%20bot", style: "LINK" }]}]});
     }
