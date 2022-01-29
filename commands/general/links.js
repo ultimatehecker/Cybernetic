f71b4bd77e6734884b3df9fe6e5d4b59200b6ccc
ultimate_hecker <rohan@halarnkar.com>
2022-01-29 11:24:39 -0500

    added a footer and a description

--- commands/general/links.js
+++ commands/general/links.js
@@ -12,7 +12,9 @@ module.exports = {
 
 		const embed = new Discord.MessageEmbed()
 			.setAuthor("Cybernetic's Various Different Links", "https://cdn.discordapp.com/avatars/879180094650863727/3040c2fb097ef6a9fb59005cab44626c.webp")
-			.setColor(colors["MainColor"]);
+			.setColor(colors["MainColor"])
+			.setDescription("This includes all of Cybernetic's links to the Support Server, Invite Link and the GitHub Repository")
+			.setFooter(`Cybernetic's links requested by ${message.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`,message.author.displayAvatarURL())
 
 		message.reply({ embeds: [embed], components: [{ type: "ACTION_ROW", components: [
 			{ type: "BUTTON", label: "Bot Invite", url: "https://discord.com/api/oauth2/authorize?client_id=834177766869565491&permissions=8&scope=applications.commands%20bot", style: "LINK" },
