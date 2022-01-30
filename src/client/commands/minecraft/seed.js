578e6ea0e89d0e3c0da85a6ec834c4e67d86a63e
ultimate_hecker <rohan@halarnkar.com>
2022-01-30 17:01:29 -0500

    new minecraft commands

---
+++ src/client/commands/minecraft/seed.js
@@ -0,0 +1,38 @@
+const colors = require("../../tools/colors.json");
+const currentDate = new Date(Date.now());
+
+module.exports = {
+    name: "seed",
+    aliases: [],
+    description: "Gives all the credits where credits is due in the making for this Discord Bot",
+    usage: "seed (type)",
+    example: "seed random",
+    async execute(client, message, args, Discord) {
+
+        await message.channel.sendTyping();
+
+        function mcseed() {
+            let random_number = Math.floor(Math.random() * 2147483647);
+            let interger = ["-", ""]
+            return interger[Math.floor(Math.random() * interger.length)], random_number
+        }
+
+        if(args[0] = "random") {
+            const seed = new Discord.MessageEmbed()
+                .setAuthor("Minecraft Seed Generator", "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp")
+                .setColor(colors["MainColor"])
+                .setDescription(`Here is your randomly generated minecraft seed: \`${mcseed()}\``)
+                .setFooter(`Random Minecarft Seed requested by ${message.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`, message.author.displayAvatarURL())
+
+            message.reply({ embeds: [seed] })
+
+        } else {
+            const error503 = new Discord.MessageEmbed()
+                .setAuthor("Error", "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp")
+                .setColor(colors["ErrorColor"])
+                .setDescription(`Currently anything other than a random seed is not-supported yet`)
+
+            message.reply({ embeds: [error503] })
+        }
+    }
+}
---
+++ src/client/commands/minecraft/server.js
@@ -0,0 +1,59 @@
+const axios = require("axios")
+const colors = require("../../tools/colors.json");
+
+module.exports = {
+    name: "server",
+    aliases: ["mcserver"],
+    description: "Will show you information about a Minecraft Server",
+    usage: "server (ip) [port]",
+    example: "server hypixel.net",
+    async execute(client, message, args, Discord) {
+
+        await message.channel.sendTyping()
+
+        try {
+            if(!args[0]) {
+                const ip404 = new Discord.MessageEmbed()
+                    .setAuthor('Error', 'https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp')
+                    .setColor(colors["MainColor"])
+                    .setDescription('You need to type in a server IP! (Example: `h!server mc.hypixel.net`)')
+                return message.reply({ embeds: [ip404], allowedMentions: { repliedUser: false } })
+            }
+
+            const MOTDData = await axios.get(`https://api.mcsrvstat.us/2/${args[0]}`).then(res => res.json());
+            const serverData = await axios.get(`https://mc-api.net/v3/server/ping/${args[0]}`).then(res => res.json());
+
+            const server = new Discord.MessageEmbed()
+                .setAuthor('Server Info', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQne0t-5uaF_3jR5ewomb8M_XfWO0qds5Qi97Tzh0hZZS7JSVWIbNZKPscUOI1FEyplpjM&usqp=CAU')
+                .setTitle(args[0])
+                .addField('IP Address', `\`${MOTDData.ip}\`:\`${MOTDData.port}\``)
+                .addField('Version', `\`${serverData.version.name}\``)
+                .addField('Online Players', `\`${serverData.players.online}\`/\`${serverData.players.max}\``)
+                .setThumbnail(serverData.favicon)
+
+                if (MOTDData.motd.clean[1] !== undefined) {
+                    const cleanMOTD = `\n ${MOTDData.motd.clean[1]}`
+                    server.addField('Clean MOTD', `\`${MOTDData.motd.clean[0]}${cleanMOTD}\``)
+
+                } else if (MOTDData.motd.clean[1] == undefined) {
+                    server.addField('Clean MOTD', `\`${MOTDData.motd.clean[0]}\``)
+                }
+
+                if (MOTDData.motd.raw[1] !== undefined) {
+                    const rawMOTD = `\n ${MOTDData.motd.raw[1]}`
+                    server.addField('Raw MOTD', `\`${MOTDData.motd.raw[0]}${rawMOTD}\``)
+                    
+                } else if (MOTDData.motd.clean[1] == undefined) {
+                    server.addField('Raw MOTD', `\`${MOTDData.motd.raw[0]}\``)
+                }
+
+            message.reply({ embeds: [server], allowedMentions: { repliedUser: false } })
+
+        } catch {
+            const error = new Discord.MessageEmbed(base)
+                .setAuthor('Error', 'https://i.imgur.com/OuoECfX.jpeg')
+                .setDescription('An error has occurred. Check the IP address. If the error persists and you are certain that the IP is correct, please make a support ticket in the server. `h!links`')
+            message.reply({ embeds: [error], allowedMentions: { repliedUser: false } })
+        }
+    }
+}
