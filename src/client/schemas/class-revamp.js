1d41161899cb045e087c930efd143b2e1b149909
ultimate_hecker <rohan@halarnkar.com>
2022-02-17 23:27:19 -0500

    REVAMP OF SETUP.JS TO CLASSES (unstable)

---
+++ src/client/schemas/class-revamp.js
@@ -0,0 +1,33 @@
+const timer = require('util').promisify(setTimeout)
+const mineflayer = require('mineflayer')
+
+class Client {
+	minecraft = null
+
+	constructor() {
+		this.minecraft = mineflayer.createBot({
+			host: process.env.IP_ADDR, 
+			username: process.env.MINECRAFT_USERNAME_ALT,       
+			auth: 'microsoft'   
+		})
+	}
+
+	getClient() {
+		return this.minecraft
+	}
+
+	afkClient = async() => {
+		while (true) {
+			createClient()
+			online.toggleStatus()
+			await timer(600000)
+			minecraft.quit()
+			online.toggleStatus()
+		}
+	}	
+
+	quitClient() {
+		this.minecraft.quit()
+	}
+	
+}
