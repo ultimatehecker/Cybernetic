59bf8eb0374625bd10fb3b265bd75304f1a78e0f
ultimate_hecker <rohan@halarnkar.com>
2022-02-17 23:32:48 -0500

    quitting command

---
+++ src/client/commands/mineflayer/quit.js
@@ -0,0 +1,45 @@
+const colors = require(`../../tools/colors.json`)
+const currentDate = new Date(Date.now());
+const setup = require(`../../schemas/setup`);
+const online = require(`../../schemas/online`)
+
+module.exports = {
+    name: "quit",
+    aliases: "quit-client",
+    description: "Quits the session of the virtualized client",
+    usage: "quit",
+    example: "quit",
+    async execute(client, message, args, Discord) {
+
+        await message.channel.sendTyping()
+
+        if(message.author.id != "724798908278112309" && message.author.id != "722092754510807133") {
+            const error = new Discord.MessageEmbed()
+                .setAuthor("Error", "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp")
+                .setColor(colors["MainColor"])
+                .setDescription("Sorry, but only the owner ultimate_hecker & poly can run this command for now")
+
+            message.reply({ embeds: [error], allowedMentions: { repliedUser: false } });
+
+        }else if(online.getStatus() != true){
+
+            const online = new Discord.MessageEmbed()
+                .setAuthor("Error", "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp")
+                .setColor(colors["MainColor"])
+                .setDescription("Sorry, but only the client as to be online to run this command")
+    
+            message.reply({ embeds: [online], allowedMentions: { repliedUser: false } });
+
+        } else {
+            const quitter = new Discord.MessageEmbed()
+                .setAuthor("Mineflayer Account Success", "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp")
+                .setColor(colors["MainColor"])
+                .setDescription(`The Minecraft account, \`${process.env.MINECRAFT_USERNAME_ALT}\`, has successfully joined the Minecraft server, \`${process.env.IP_ADDR}\``)
+                .setFooter(`Mineflayer Services requested by ${message.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`, message.author.displayAvatarURL())
+
+            message.reply({ embeds: [quitter], allowedMentions: { repliedUser: false } });
+            setup.quitClient()
+            online.toggleStatus()
+        }
+    }
+}
