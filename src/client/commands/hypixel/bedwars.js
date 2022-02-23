effd8c0fc0d14e71871550b2d21f76b5e9ea5bd4
ultimate_hecker <rohan@halarnkar.com>
2022-02-23 18:24:40 -0500

    added averages & fixed lootChests returning undef

--- src/client/commands/hypixel/bedwars.js
+++ src/client/commands/hypixel/bedwars.js
@@ -40,11 +40,12 @@ module.exports = {
                     .setThumbnail(`https://crafatar.com/avatars/${player.uuid}?overlay&size=256`)
                     .setFooter(`${player.nickname}'s Bedwars Statistics requested by ${message.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`, message.author.displayAvatarURL())
 
-                    .addField("General Stats", `\`•\` **Levels**: \`${player.stats.bedwars.level}✫\` \n \`•\` **Coins**: \`${commaNumber(player.stats.bedwars.coins)}\``, true)
+                    .addField("General Stats", `\`•\` **Levels**: \`${player.stats.bedwars.level}✫\` \n \`•\` **Coins**: \`${commaNumber(player.stats.bedwars.coins)}\` \n \`•\` **Loot Chest**: \`${commaNumber((player.stats.bedwars.lootChests.normal + player.stats.bedwars.lootChests.golden + player.stats.bedwars.lootChests.christmas + player.stats.bedwars.lootChests.easter + player.stats.bedwars.lootChests.halloween))}\``, true)
 					.addField("Games", `\`•\` **Winstreak**: \`${commaNumber(player.stats.bedwars.winstreak)}\` \n \`•\` **Wins**: \`${commaNumber(player.stats.bedwars.wins)}\` \n \`•\` **Losses**: \`${commaNumber(player.stats.bedwars.losses)}\` \n \`•\` **WLR**: \`${player.stats.bedwars.WLRatio}\``, true)
 					.addField("Combat", `\`•\` **Kills**: \`${commaNumber(player.stats.bedwars.kills)}\` \n \`•\` **Deaths**: \`${commaNumber(player.stats.bedwars.deaths)}\` \n \`•\` **KDR**: \`${player.stats.bedwars.KDRatio}\``, true)
 					.addField("Finals", `\`•\` **Kills**: \`${commaNumber(player.stats.bedwars.finalKills)}\` \n \`•\` **Deaths**: \`${commaNumber(player.stats.bedwars.finalDeaths)}\` \n \`•\` **FKDR**: \`${player.stats.bedwars.finalKDRatio}\``, true)
 					.addField("Beds", `\`•\` **Broken**: \`${commaNumber(player.stats.bedwars.beds.broken)}\` \n \`•\` **Lost**: \`${commaNumber(player.stats.bedwars.beds.lost)}\` \n \`•\` **BBLR**: \`${player.stats.bedwars.beds.BLRatio}\``, true)
+                    .addField("Averages per Game", `\`•\` **Kills**: \`${commaNumber(Math.ceil(player.stats.bedwars.kills / player.stats.bedwars.playedGames))}\` \n \`•\` **Final Kills**: \`${commaNumber(Math.ceil(player.stats.bedwars.finalKills / player.stats.bedwars.playedGames))}\` \n \`•\` **Beds Broken**: \`${commaNumber(Math.ceil(player.stats.bedwars.beds.broken / player.stats.bedwars.playedGames))}\``, true)
 					.addField("Milestones", `\`•\` **Wins to ${commaNumber(Math.ceil(player.stats.bedwars.WLRatio))} WLR**: \`${commaNumber(player.stats.bedwars.losses * Math.ceil(player.stats.bedwars.WLRatio) - player.stats.bedwars.wins)}\` \n \`•\` **Finals to ${commaNumber(Math.ceil(player.stats.bedwars.finalKDRatio))} FKDR**: \`${commaNumber(player.stats.bedwars.finalDeaths * Math.ceil(player.stats.bedwars.finalKDRatio) - player.stats.bedwars.finalKills)}\` \n \`•\` **Beds to ${commaNumber(Math.ceil(player.stats.bedwars.beds.BLRatio))} BBLR**: \`${commaNumber(player.stats.bedwars.beds.lost * Math.ceil(player.stats.bedwars.beds.BLRatio) - player.stats.bedwars.beds.broken)}\``, true)
 
                 message.reply({ embeds: [bedwars], allowedMentions: { repliedUser: false } })
@@ -66,7 +67,7 @@ module.exports = {
                     const neverPlayed = new Discord.MessageEmbed()
                         .setAuthor('Error', 'https://cdn.discordapp.com/avatars/879180094650863727/3040c2fb097ef6a9fb59005cab44626c.webp')
                         .setColor(colors["ErrorColor"])
-                        .setDescription('That player has never played this game')
+                        .setDescription("That player has never played this game")
                     return message.reply({ embeds: [neverPlayed], allowedMentions: { repliedUser: false } })
                 } else {
 					const error = new Discord.MessageEmbed()
