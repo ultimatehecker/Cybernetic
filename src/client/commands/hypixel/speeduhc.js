28f0d6e15db0805a661ee2a1df5f146c0f4a0d4c
ultimate_hecker <rohan@halarnkar.com>
2022-03-12 12:11:08 -0500

    added uhc commands

---
+++ src/client/commands/hypixel/speeduhc.js
@@ -0,0 +1,94 @@
+const { hypixel, errors } = require('../../schemas/hypixel');
+const commaNumber = require('comma-number');
+const User = require('../../schemas/user');
+const colors = require("../../tools/colors.json");
+const currentDate = new Date(Date.now());
+
+module.exports = {
+    name: 'speeduhc',
+    aliases: ['suhc'],
+    description: 'Shows you the SpeedUHC Statistics of an average Hypixel Player!',
+    usage: 'speeduhc [IGN]',
+    example: 'speeduhc ultimate_hecker',
+    async execute(client, message, args, Discord, prefix) {
+
+        await message.channel.sendTyping()
+
+        let authorError = {
+            name: "Error",
+            iconURL: "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp"
+        }
+    
+        let authorSuccess = {
+            name: "SpeedUHC Statistics",
+            iconURL: "https://cdn.discordapp.com/avatars/923947315063062529/0a3bc17096585739484e4c6dfb7c184b.webp"
+        }
+
+        const data = await User.findOne({
+            id: message.author.id
+        });
+
+        if (!data && !args[0]) { // if someone didn't type in ign
+            const ign404 = new Discord.MessageEmbed()
+                .setAuthor(authorError)
+                .setColor(colors["ErrorColor"])
+                .setDescription(`You need to type in a player's IGN! (Example: \`${prefix}speeduhc ultimate_hecker\`) \nYou can also link your account to do commands without inputting an IGN. (Example: \`${prefix}link ultimate_hecker\`)`)
+            return message.reply({ embeds: [ign404] })
+        }
+
+        let player;
+
+        if (data && !args[0]) {
+             player = data.uuid;
+        } else if (args[0]) {
+             player = args[0];
+        }
+
+        hypixel.getPlayer(player).then((player) => {
+            const speeduhc = new Discord.MessageEmbed()
+                .setAuthor(authorSuccess)
+                .setTitle(`[${player.rank}] ${player.nickname}`)
+                .setColor(colors["MainColor"])
+                .setThumbnail(`https://crafatar.com/avatars/${player.uuid}?overlay&size=256`)
+                .setFooter(`SpeedUHC Statistics requested by ${message.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`,message.author.displayAvatarURL())
+                .addField('Kills', `\`${commaNumber(player.stats.speedUHC.kills)}\``, true)
+                .addField('Losses', `\`${commaNumber(player.stats.speedUHC.losses)}\``, true)
+                .addField('Wins', `\`${commaNumber(player.stats.speedUHC.wins)}\``, true)
+                .addField('Winstreak', `\`${commaNumber(player.stats.speedUHC.winstreak)}\``, true)
+                .addField('Deaths', `\`${commaNumber(player.stats.speedUHC.deaths)}\``, true)
+                .addField('Games Played', `\`${commaNumber(player.stats.speedUHC.playedGames)}\``, true)
+                .addField('Coins', `\`${commaNumber(player.stats.speedUHC.coins)}\``, true)
+                .addField('KD Ratio', `\`${player.stats.speedUHC.KDRatio}\``, true)
+                .addField('WL Ratio', `\`${player.stats.speedUHC.WLRatio}\``, true)
+
+            message.reply({ embeds: [speeduhc], allowedMentions: { repliedUser: false } });
+
+        }).catch(e => { // error messages
+            if (e.message === errors.PLAYER_DOES_NOT_EXIST) {
+                const player404 = new Discord.MessageEmbed()
+                    .setAuthor(authorError)
+                    .setColor(colors["ErrorColor"])
+                    .setDescription('I could not find that player in the API. Check spelling and name history.')
+                return message.reply({ embeds: [player404], allowedMentions: { repliedUser: false } })
+            } else if (e.message === errors.PLAYER_HAS_NEVER_LOGGED) {
+                const neverLogged = new Discord.MessageEmbed()
+                    .setAuthor(authorError)
+                    .setColor(colors["ErrorColor"])
+                    .setDescription('That player has never logged into Hypixel.')
+                return message.reply({ embeds: [neverLogged], allowedMentions: { repliedUser: false } })
+            } else if (e.message === errors.CANNOT_READ_PROPERTIES_OF_UNDEFINED) {
+                const neverLogged = new Discord.MessageEmbed()
+                    .setAuthor(authorError)
+                    .setColor(colors["ErrorColor"])
+                    .setDescription('That player has never played this game')
+                return message.reply({ embeds: [neverLogged], allowedMentions: { repliedUser: false } })
+            } else {
+                const error = new Discord.MessageEmbed()
+                    .setAuthor(authorError)
+                    .setColor(colors["ErrorColor"])
+                    .setDescription(`A problem has been detected and the command has been aborted, if this is the first time seeing this, check the error message for more details, if this error appears multiple times, DM \`ultiamte_hecker#1165\` with this error message \n \n \`Error:\` \n \`\`\`${e}\`\`\``)
+                return message.reply({ embeds: [error], allowedMentions: { repliedUser: false } })
+            }       
+        });
+    }
+}
