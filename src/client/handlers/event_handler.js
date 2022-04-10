87f25f8a6dfa7c17b961b8d5ad13bedd0a97514b
ultimate_hecker <rohan@halarnkar.com>
2022-04-09 22:16:28 -0400

    missed one period

--- src/client/handlers/event_handler.js
+++ src/client/handlers/event_handler.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports = (client, Discord) => {
 	const load_dir = (dirs) => {
 		const event_files = fs
-			.readdirSync(`./events/${dirs}`)
+			.readdirSync(`./src/client/events/${dirs}`)
 			.filter((file) => file.endsWith("js"));
 
 		for (const file of event_files) {
