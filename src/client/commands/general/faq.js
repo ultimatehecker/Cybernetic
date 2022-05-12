9e2b541e4e367ea1ae8b91626a8b4e4a1a2f704f
ultimate_hecker <rohan@halarnkar.com>
2022-05-12 16:23:17 -0400

    changed description

--- src/client/commands/general/faq.js
+++ src/client/commands/general/faq.js
@@ -3,7 +3,7 @@ const colors = require("../../tools/colors.json");
 module.exports = {
     name: "faq",
     aliases: [],
-    description: "Gives all the credits where credits is due in the making for this Discord client",
+    description: "Gives you common questions asked about Cybernetic",
 	options: [],
     defaultPermission: true,
     usage: "faq",
@@ -17,12 +17,12 @@ module.exports = {
             iconURL: "https://cdn.discordapp.com/app-icons/951969820130300015/588349026faf50ab631528bad3927345.png?size=256"
         }
 
-        const credits = new Discord.MessageEmbed()
+        const faq = new Discord.MessageEmbed()
             .setAuthor(author)
             .setColor(colors["MainColor"])
-            .setDescription(`Not currently working, come back later...`)
+            .setDescription(`When will the full version of Cybernetic be released? \n *The full version of Cybernetic will probably be released when skyblock commands have been added, because then there will be a good reason to release it and there will be still more that could be added* \n \n What will your response be to the new message intent being removed for verified bots? \n *When I fully release the bot, it will still have the message based commands, but if it gains enough traction and interest, there will be a reason to verify it and then move it to solely interaction based commands*`)
 
-        return message.reply({ embeds: [credits], allowedMentions: { repliedUser: true } });
+        return message.reply({ embeds: [faq], allowedMentions: { repliedUser: true } });
     },
     async slashExecute(client, Discord, interaction){
 
@@ -33,11 +33,11 @@ module.exports = {
             iconURL: "https://cdn.discordapp.com/app-icons/951969820130300015/588349026faf50ab631528bad3927345.png?size=256"
         }
 
-		const embed = new Discord.MessageEmbed()
+		const faq = new Discord.MessageEmbed()
 			.setAuthor(author)
 			.setColor(colors["MainColor"])
-			.setDescription(`Not currently working, come back later...`)
+			.setDescription(`When will the full version of Cybernetic be released? \n *The full version of Cybernetic will probably be released when skyblock commands have been added, because then there will be a good reason to release it and there will be still more that could be added* \n \n What will your response be to the new message intent being removed for verified bots? \n *When I fully release the bot, it will still have the message based commands, but if it gains enough traction and interest, there will be a reason to verify it and then move it to solely interaction based commands*`)
 
-		interaction.editReply({ embeds: [embed], allowedMentions: { repliedUser: true } });
+		interaction.editReply({ embeds: [faq], allowedMentions: { repliedUser: true } });
 	}
 }
