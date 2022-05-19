2f7aef4f23b3350f36e8b9d1b95e2d462189f972
ultimate_hecker <rohan@halarnkar.com>
2022-05-18 21:48:40 -0400

    fixed different fucntion

--- src/client/events/client/interactionCreate.js
+++ src/client/events/client/interactionCreate.js
@@ -3,5 +3,5 @@ const handleComponent = require('./interactions/component.js');
 
 module.exports = (Discord, client, interaction) => {
     if(interaction.isCommand()) return handleCommand(Discord, client, interaction)
-    if(interaction.isComponent())  return handleComponent(Discord, client, interaction)
+    if(interaction.isMessageComponent())  return handleComponent(Discord, client, interaction)
 };
