e57064c3e8ed266f43e653d7f03e3a4eee8cb465
ultimate_hecker <rohan@halarnkar.com>
2022-05-18 21:44:18 -0400

    added options

--- src/client/commands/general/rng.js
+++ src/client/commands/general/rng.js
@@ -5,6 +5,15 @@ module.exports = {
 	name: "rng",
 	aliases: ["random"],
 	description: "Generates a random number from an array number from what you supply",
+	options: [
+		{ 
+			name: "number",
+			description: "First number for the random number picker",
+			required: true,
+			type: "STRING"
+		}
+	],
+    defaultPermission: true,
 	usage: "`rng [Min] [Max]`",
 	example: "`rng 1 10`",
 	async execute(client, message, args, Discord, prefix) {
@@ -58,12 +67,22 @@ module.exports = {
 	async slashExecute(client, Discord, interaction, serverDoc) {
 
 		await interaction.deferReply({ ephemeral: false });
+
+		let authorError = {
+            name: "Error",
+            iconURL: "https://cdn.discordapp.com/app-icons/951969820130300015/588349026faf50ab631528bad3927345.png?size=256"
+        }
+
+        let authorSuccess = {
+            name: "RNG",
+            iconURL: "https://cdn.discordapp.com/app-icons/951969820130300015/588349026faf50ab631528bad3927345.png?size=256"
+        }
 		
 		let response = [Math.floor(Math.random() * (interaction.options.get("number") - 1 + 1) + 1)]; // rng
 
 		if(!interaction.options.get("number")){
 			const nonumbers = new Discord.MessageEmbed()
-				.setAuthor("Error", "https://cdn.discordapp.com/avatars/879180094650863727/3040c2fb097ef6a9fb59005cab44626c.webp")
+				.setAuthor(authorError)
 				.setColor(colors["ErrorColor"])
 				.setDescription(`You didn't specify a lowest and highest number! (Example: \`${serverDoc.prefix}rng 1 10\`)`)
 
@@ -76,7 +95,7 @@ module.exports = {
 
 		if(isNaN(interaction.options.get("number"))){
 			const nan = new Discord.MessageEmbed()
-				.setAuthor("Error", "https://cdn.discordapp.com/avatars/879180094650863727/3040c2fb097ef6a9fb59005cab44626c.webp")
+				.setAuthor(authorError)
 				.setColor(colors["ErrorColor"])
 				.setDescription("That is not a number!")
 
@@ -88,7 +107,7 @@ module.exports = {
 		}
 
 		const embed = new Discord.MessageEmbed()
-			.setAuthor("RNG", "https://cdn.discordapp.com/avatars/879180094650863727/3040c2fb097ef6a9fb59005cab44626c.webp")
+			.setAuthor(authorSuccess)
 			.setColor(colors["MainColor"])
 			.setDescription(`You got \`${response}\`!`)
 			
