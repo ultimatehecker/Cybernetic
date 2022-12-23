1943a82aec99b598839428107237512c7f5f6731
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2022-12-23 11:28:22 -0500

    added unmute command

---
+++ src/client/commands/moderation/unmute.js
@@ -0,0 +1,191 @@
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require("discord.js");
+const colors = require("../../tools/colors.json");
+
+module.exports = {
+	name: "unmute",
+	aliases: [],
+	description: "Unmutes the mentioned user",
+	options: [
+		{
+			name: "user",
+			description: "The user you want to unmute",
+			type: ApplicationCommandOptionType.User,
+			required: true,
+		},
+		{
+			name: "reason",
+			description: 'The reason for unmuting this user - defaults to "Unmuted by <your tag>" if omitted',
+			type: ApplicationCommandOptionType.String,
+			required: false,
+		},
+	],
+	defaultPermission: true,
+	usage: "unmute (user) [reason]",
+	example: "unmute @ultimate_hecker not spamming",
+	async execute(client, message, args, Discord) {
+
+		await message.channel.sendTyping();
+
+        let authorError = {
+            name: "Error",
+            iconURL: "https://cdn.discordapp.com/app-icons/951969820130300015/588349026faf50ab631528bad3927345.png?size=256"
+        }
+
+        let authorSuccess = {
+            name: "Success",
+            iconURL: "https://cdn.discordapp.com/app-icons/951969820130300015/588349026faf50ab631528bad3927345.png?size=256"
+        }
+
+		let user = message.mentions.users.first();
+
+		if (!user) {
+			user = client.utils.resolveTag(message.guild, args[0]);
+		}
+
+		if (user) {
+			const member = message.guild.members.resolve(user);
+			if (!member.roles.cache.find((role) => role.name === "Muted")) {
+				const embed = new Discord.EmbedBuilder()
+					.setAuthor(authorError)
+					.setColor(colors["ErrorColor"])
+					.setDescription("This member is not muted!")
+
+				return message.reply({ embeds: [embed], allowedMentions: { repliedUser: true } }).then((sent) => {
+					setTimeout(() => {
+						sent.delete();
+					}, 5000);
+				});
+			}
+
+			const unmuter = message.guild.members.resolve(message.author);
+
+			if (member) {
+				if (!unmuter.permissions.has(PermissionFlagsBits.MuteMembers)) {
+					const permsEmbed = new Discord.EmbedBuilder()
+						.setAuthor(authorError)
+						.setColor(colors["ErrorColor"])
+						.setDescription("You do not have permission to unmute!")
+
+					return message.reply({ embeds: [permsEmbed], allowedMentions: { repliedUser: true } }).then((sent) => {
+						setTimeout(() => {
+							sent.delete();
+						}, 5000);
+					});
+				}
+
+				const unmuteRole = message.guild.roles.cache.find((role) => role.name === "Muted");
+
+				member.roles.remove(unmuteRole, args[1]).then(() => {
+					const embed = new Discord.EmbedBuilder()
+						.setAuthor(authorSuccess)
+						.setColor(colors["MainColor"])
+						.setDescription(`Successfully unmuted **${user.tag}**`);
+
+					message.reply({ embeds: [embed], allowedMentions: { repliedUser: true } });
+				}).catch((err) => {
+					const embed = new Discord.EmbedBuilder()
+						.setAuthor(authorError)
+						.setColor(colors["ErrorColor"])
+						.setDescription(`A problem has been detected and the command has been aborted, if this is the first time seeing this, check the error message for more details, if this error appears multiple times, DM \`ultiamte_hecker#1165\` with this error message \n \n \`Error:\` \n \`\`\`${err}\`\`\``)
+
+					message.reply({ embeds: [embed], allowedMentions: { repliedUser: true } }).then((sent) => {
+						setTimeout(() => {
+							sent.delete();
+						}, 5000);
+					});
+				});
+			} else {
+				const embed = new Discord.EmbedBuilder()
+					.setAuthor(authorError)
+					.setColor(colors["ErrorColor"])
+					.setDescription("That user isn't in this guild!");
+
+				message.reply({ embeds: [embed], allowedMentions: { repliedUser: true } }).then((sent) => {
+					setTimeout(() => {
+						sent.delete();
+					}, 5000);
+				});
+			}
+		} else {
+			const embed = new Discord.EmbedBuilder()
+				.setAuthor(authorError)
+				.setColor(colors["ErrorColor"])
+				.setDescription("You didn't mention the user to unmute!");
+
+			message.reply({ embeds: [embed], allowedMentions: { repliedUser: true } }).then((sent) => {
+				setTimeout(() => {
+					sent.delete();
+				}, 5000);
+			});
+		}
+	},
+	async slashExecute(client, Discord, interaction) {
+
+		await interaction.deferReply({ emphemeral: false });
+
+		let authorError = {
+            name: "Error",
+            iconURL: "https://cdn.discordapp.com/app-icons/923947315063062529/588349026faf50ab631528bad3927345.png?size=256"
+        }
+    
+        let authorSuccess = {
+            name: "Success",
+            iconURL: "https://cdn.discordapp.com/app-icons/923947315063062529/588349026faf50ab631528bad3927345.png?size=256"
+        }
+		
+		let user = interaction.options.get("user").user;
+
+		const member = interaction.guild.members.resolve(user);
+
+		if (!member.roles.cache.find((role) => role.name === "Muted")) {
+			const embed = new Discord.EmbedBuilder()
+				.setAuthor(authorError)
+				.setColor(colors["ErrorColor"])
+				.setDescription("This member is not muted!")
+
+			return interaction.editReply({ embeds: [embed], allowedMentions: { repliedUser: true } }).then(() => {
+				setTimeout(function() {
+					interaction.deleteReply()
+				}, 5000);
+			});
+		}
+
+		const unmuter = interaction.member;
+
+		if (!unmuter.permissions.has(PermissionFlagsBits.MuteMembers)) {
+			const permsEmbed = new Discord.EmbedBuilder()
+				.setAuthor(authorError)
+				.setColor(colors["ErrorColor"])
+				.setDescription("You do not have permission to unmute!")
+
+			return interaction.editReply({ embeds: [permsEmbed], allowedMentions: { repliedUser: true } }).then(() => {
+				setTimeout(function() {
+					interaction.deleteReply()
+				}, 5000);
+			});
+		}
+
+		const unmuteRole = interaction.guild.roles.cache.find((role) => role.name === "Muted");
+
+		member.roles.remove(unmuteRole, interaction.options.get("reason")?.value ?? `User unmuted by ${interaction.user.tag}`).then(() => {
+			const embed = new Discord.EmbedBuilder()
+				.setAuthor(authorSuccess)
+				.setColor(colors["MainColor"])
+				.setDescription(`Successfully unmuted **${user.tag}**`);
+
+			interaction.editReply({ embeds: [embed], allowedMentions: { repliedUser: true } });
+		}).catch((err) => {
+			const embed = new Discord.EmbedBuilder()
+				.setAuthor(authorError)
+				.setColor(colors["ErrorColor"])
+				.setDescription(`A problem has been detected and the command has been aborted, if this is the first time seeing this, check the error message for more details, if this error appears multiple times, DM \`ultiamte_hecker#1165\` with this error message \n \n \`Error:\` \n \`\`\`${err}\`\`\``);
+
+			interaction.editReply({ embeds: [embed], allowedMentions: { repliedUser: true } }).then(() => {
+				setTimeout(function() {
+					console.error(err);
+					interaction.deleteReply()
+				}, 5000);
+			});
+		});
+	},
+};
