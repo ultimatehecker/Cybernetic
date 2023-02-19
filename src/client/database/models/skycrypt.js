352a488818bd751f4825cc38a6acbef325bf2754
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-02-19 13:07:55 -0500

    fixed invalid player and added profileskills

--- src/client/database/models/skycrypt.js
+++ src/client/database/models/skycrypt.js
@@ -1,15 +1,18 @@
 const axios = require('axios');
 const skycryptUrl = 'https://sky.shiiyu.moe/api/v2'
 
-async function getSkyblock(ign, profileName) {
-    let { profile } = (await axios.get(`${skycryptUrl}/profile/${ign}`))
-    let { talismen } = (await axios.get(`${skycryptUrl}/talismans/${ign}`)) // maybe add the other option
-    let { slayers } = (await axios.get(`${skycryptUrl}/slayers/${ign}`)) // maybe add the other option
-    let { coins } = (await axios.get(`${skycryptUrl}/coins/${ign}`)) // maybe add the other option
+async function getSkyblock(player, profileName) {
+    let { profiles } = (await axios.get(`${skycryptUrl}/profile/${player}`))
+    let { talismen } = (await axios.get(`${skycryptUrl}/talismans/${player}`)) // maybe add the other option
+    let { slayers } = (await axios.get(`${skycryptUrl}/slayers/${player}`)) // maybe add the other option
+    let { coins } = (await axios.get(`${skycryptUrl}/coins/${player}`)) // maybe add the other option
     let { bazaar } = (await axios.get(`${skycryptUrl}/bazaar`)) 
-    let { dungeons } = (await axios.get(`${skycryptUrl}/dungeons/${ign}`)) // maybe add the other option
+    let { dungeons } = (await axios.get(`${skycryptUrl}/dungeons/${player}`)) // maybe add the other option
+    let profileSkills = require(`../../tools/skyblock.json`)
 
-    let skyblock = { profile, talismen, slayers, coins, bazaar, dungeons }
+    const profile = Object.values(profiles).find(profile => profile.current);
+
+    let skyblock = { profile, talismen, slayers, coins, bazaar, dungeons, profileSkills }
     return skyblock
 }
 
