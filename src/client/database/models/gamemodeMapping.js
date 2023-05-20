d86ee1c0e9a2938b3e5b11d8124aac6e3a006f0c
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-05-20 17:46:07 -0400

    gamemodeMapping for gamemodes in bedwars

---
+++ src/client/database/models/gamemodeMapping.js
@@ -0,0 +1,20 @@
+let gamemodeMapping = {
+    "4v4": "two_four_",
+    "castle": "castle_",
+    "fours": "four_four_",
+    "threes": "four_three_",
+    "doubles": "eight_two_",
+    "solo": "eight_one_"
+}
+
+let dreamMapping = {
+    "ultimate": "four_four_",
+    "rush": "four_three_",
+    "lucky": "eight_two_",
+    "armed": "armed_",
+    "voidless": "voidless_",
+    "underworld": "underworld_",
+}
+
+exports.gamemodeMapping = gamemodeMapping
+exports.dreamMapping = dreamMapping
